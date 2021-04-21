2ac0f9cb1326047f7d8680f1546eb15f23cb4144
liujiazhen <jzliu@thoughtworks.com>
2021-04-21 16:52:41 +0800

    [jzliu] add k8s cluster terraform

---
+++ infrastructure/cluster.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = "1.121.1"
+    }
+  }
+}
+
+provider "alicloud" {
+}
+
+
+locals {
+  k8s_name          = "tf-cluster-poc"
+  new_vpc_name      = "vpc-for-${local.k8s_name}"
+  log_project_name  = "log-for-${local.k8s_name}"
+  db_instance_name  = "poc-tw-test-db"
+  db_name           = "test-db"
+}
+
+data "alicloud_instance_types" "default" {
+  cpu_core_count       = 4
+  memory_size          = 8
+  kubernetes_node_role = "Worker"
+}
+
+data "alicloud_zones" "default" {
+  available_instance_type = data.alicloud_instance_types.default.instance_types[0].id
+}
+
+//VPC
+resource "alicloud_vpc" "default" {
+  count      = 1
+  vpc_name   = local.new_vpc_name
+  cidr_block = "192.168.0.0/16"
+}
+
+resource "alicloud_vswitch" "vswitches" {
+  count             = 3
+  vpc_id            = alicloud_vpc.default[0].id
+  cidr_block        = element(["192.168.0.0/19", "192.168.64.0/19", "192.168.96.0/20"], count.index)
+  zone_id           = element(data.alicloud_zones.default.zones.*.id, count.index)
+}
+
+//RDS
+resource "alicloud_db_instance" "instance" {
+  engine           = "MySQL"
+  engine_version   = "8.0"
+  instance_type    = "rds.mysql.s1.small"
+  instance_storage = "10"
+  vswitch_id       = alicloud_vswitch.vswitches[2].id
+  instance_name    = local.db_instance_name
+}
+
+resource "alicloud_db_database" "default" {
+  instance_id = alicloud_db_instance.instance.id
+  name        = local.db_name
+}
+
+//ACK
+resource "alicloud_log_project" "log" {
+  name        = local.log_project_name
+  description = "created by terraform for managedkubernetes cluster"
+}
+
+resource "alicloud_cs_managed_kubernetes" "k8s" {
+
+  name                      = local.k8s_name
+
+  version                   = "1.18.8-aliyun.1"
+
+  cluster_spec              = "ack.pro.small"
+
+  rds_instances             = [alicloud_db_instance.instance.id]
+
+  worker_vswitch_ids        = split(",", join(",", alicloud_vswitch.vswitches.*.id))
+
+  new_nat_gateway           = true
+
+  worker_instance_types     = [data.alicloud_instance_types.default.instance_types[0].id]
+
+  worker_number             = 3
+
+  password                  = "Yourpassword1234"
+
+  pod_cidr                  = "172.20.0.0/16"
+
+  service_cidr              = "172.21.0.0/20"
+
+  install_cloud_monitor     = true
+
+  slb_internet_enabled      = true
+
+  worker_disk_category      = "cloud_efficiency"
+
+  runtime = {
+    name = "docker"
+    version = "19.03.5"
+  }
+
+  addons {
+    name     = "logtail-ds"
+    config   = "{\"IngressDashboardEnabled\":\"true\",\"sls_project_name\":alicloud_log_project.log.name}"
+  }
+}
---
+++ infrastructure/outputs.tf
@@ -0,0 +1,87 @@
+// Output VPC resource
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = alicloud_cs_managed_kubernetes.k8s.vpc_id
+}
+
+output "vswitch_ids" {
+  description = "List ID of the VSwitches."
+  value       = [alicloud_cs_managed_kubernetes.k8s.vswitch_ids]
+}
+
+// Output kubernetes resource
+output "cluster_id" {
+  description = "ID of the kunernetes cluster."
+  value       = alicloud_cs_managed_kubernetes.k8s.id
+}
+
+output "cluster_name" {
+  description = "Name of the kunernetes cluster."
+  value       = alicloud_cs_managed_kubernetes.k8s.name
+}
+
+output "master_slb_intranet" {
+  description = "The ID of private load balancer where the current cluster master node is located."
+  value       = alicloud_cs_managed_kubernetes.k8s.slb_intranet
+}
+
+output "security_group_id" {
+  description = "The ID of security group where the current cluster worker node is located."
+  value       = alicloud_cs_managed_kubernetes.k8s.security_group_id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of nat gateway used to launch kubernetes cluster."
+  value       = alicloud_cs_managed_kubernetes.k8s.nat_gateway_id
+}
+
+output "worker_nodes" {
+  description = "List worker nodes of cluster."
+  value       = [alicloud_cs_managed_kubernetes.k8s.worker_nodes]
+}
+
+output "connection_info" {
+  description = "Map of kubernetes cluster connection information."
+  value       = [alicloud_cs_managed_kubernetes.k8s.connections]
+}
+
+output "version" {
+  description = "The Kubernetes server version for the cluster."
+  value       = alicloud_cs_managed_kubernetes.k8s.version
+}
+
+output "worker_ram_role_name" {
+  description = "The RamRole Name attached to worker node."
+  value       = alicloud_cs_managed_kubernetes.k8s.worker_ram_role_name
+}
+
+output "certificate_authority" {
+  description = "Nested attribute containing certificate authority data for your cluster."
+  value       = alicloud_cs_managed_kubernetes.k8s.certificate_authority
+}
+
+//Output RDS resource
+output "instance_id" {
+  description = "The RDS instance ID."
+  value = alicloud_db_instance.instance.id
+}
+
+output "rds_connection_port" {
+  description = "RDS database connection port."
+  value = alicloud_db_instance.instance.port
+}
+
+output "rds_connection_string" {
+  description = "RDS database connection string."
+  value = alicloud_db_instance.instance.connection_string
+}
+
+output "rds_ssl_status" {
+  description = "Status of the SSL feature. Yes: SSL is turned on; No: SSL is turned off."
+  value = alicloud_db_instance.instance.ssl_status
+}
+
+output "database_id" {
+  description = "The current database resource ID. Composed of instance ID and database name with format <instance_id>:<name>."
+  value = alicloud_db_database.default.id
+}
