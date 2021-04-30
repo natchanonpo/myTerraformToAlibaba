26466f3f1fedb8dac64308eb93510fa9c3ca2c00
Nud Teeraworamongkol <nud.teeraworamongkol@exxonmobil.com>
2021-04-30 16:13:59 +0700

    Remove cluster_addons.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,3 +7,10 @@ variable "resource_group_id" {
   type    = string
   default = "rg-aekzr7sprapjh4a"
 }
+
+variable "cluster_addons" {
+  type = list(object({
+    name   = string
+    config = string
+  }))
+}
