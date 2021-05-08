c0d8fe835b0f79367f284438d9f5d978bfe0d878
liujiazhen <jzliu@thoughtworks.com>
2021-05-08 17:02:13 +0800

    [jzliu] #PBI-4 fix format

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -8,18 +8,18 @@ locals {
 }
 
 resource "alicloud_db_instance" "rds_instance" {
-  resource_group_id = var.resource_group_id
-  engine            = "MySQL"
-  engine_version    = "8.0"
-  instance_type     = var.instance_type
-  instance_storage  = var.instance_storage
-  vswitch_id        = var.vswitch_id
-  instance_name     = var.instance_name
-  tags              = var.tags
+  resource_group_id    = var.resource_group_id
+  engine               = "MySQL"
+  engine_version       = "8.0"
+  instance_type        = var.instance_type
+  instance_storage     = var.instance_storage
+  vswitch_id           = var.vswitch_id
+  instance_name        = var.instance_name
+  tags                 = var.tags
   instance_charge_type = "PrePaid"
-  period            = 1
-  auto_renew        = true
-  auto_renew_period = 1
+  period               = 1
+  auto_renew           = true
+  auto_renew_period    = 1
 }
 
 resource "alicloud_db_database" "database" {
