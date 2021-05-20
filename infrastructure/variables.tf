78d5d17e59bd386b1d85d579301b83e80a19b841
natchanonpo <natchanon.pornprasatpol@exxonmobil.com>
2021-05-20 13:05:12 +0700

    fix variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,33 +6,33 @@ variable "project_name" {
   type = string
 }
 
-variable "vpc_cidr" {
-  type = string
-}
+# variable "vpc_cidr" {
+#   type = string
+# }
 
-variable "ecs_vswitch_cidrs" {
-  type = list(string)
-}
+# variable "ecs_vswitch_cidrs" {
+#   type = list(string)
+# }
 
-variable "rds_vswitch_cidrs" {
-  type = list(string)
-}
+# variable "rds_vswitch_cidrs" {
+#   type = list(string)
+# }
 
-variable "kafka_vswitch_cidrs" {
-  type = list(string)
-}
+# variable "kafka_vswitch_cidrs" {
+#   type = list(string)
+# }
 
-variable "ecs_vswitch_zone_ids" {
-  type = list(string)
-}
+# variable "ecs_vswitch_zone_ids" {
+#   type = list(string)
+# }
 
-variable "rds_vswitch_zone_ids" {
-  type = list(string)
-}
+# variable "rds_vswitch_zone_ids" {
+#   type = list(string)
+# }
 
-variable "kafka_vswitch_zone_ids" {
-  type = list(string)
-}
+# variable "kafka_vswitch_zone_ids" {
+#   type = list(string)
+# }
 
 variable "rds_instance_type" {
   type = string
@@ -54,72 +54,72 @@ variable "db_allowed_external_envs" {
   type = list(string)
 }
 
-variable "db_xom_editor_password" {
-  type = string
-}
+# variable "db_xom_editor_password" {
+#   type = string
+# }
 
-variable "db_xom_readonly_password" {
-  type = string
-}
+# variable "db_xom_readonly_password" {
+#   type = string
+# }
 
-variable "db_external_editor_password" {
-  type = string
-}
+# variable "db_external_editor_password" {
+#   type = string
+# }
 
-variable "db_external_readonly_password" {
-  type = string
-}
+# variable "db_external_readonly_password" {
+#   type = string
+# }
 
-variable "kafka_disk_size" {
-  type = number
-}
+# variable "kafka_disk_size" {
+#   type = number
+# }
 
-variable "kafka_topic_quota" {
-  type = number
-}
+# variable "kafka_topic_quota" {
+#   type = number
+# }
 
-variable "kafka_io_max" {
-  type = number
-}
+# variable "kafka_io_max" {
+#   type = number
+# }
 
-variable "kafka_eip_max" {
-  type = number
-}
+# variable "kafka_eip_max" {
+#   type = number
+# }
 
-variable "k8s_cluster_spec" {
-  type = string
-}
+# variable "k8s_cluster_spec" {
+#   type = string
+# }
 
-variable "k8s_worker_instance_type" {
-  type = string
-}
+# variable "k8s_worker_instance_type" {
+#   type = string
+# }
 
-variable "k8s_worker_disk_size" {
-  type = string
-}
+# variable "k8s_worker_disk_size" {
+#   type = string
+# }
 
-variable "k8s_load_balancer_spec" {
-  type = string
-}
+# variable "k8s_load_balancer_spec" {
+#   type = string
+# }
 
-variable "k8s_pod_cidr" {
-  type = string
-}
+# variable "k8s_pod_cidr" {
+#   type = string
+# }
 
-variable "k8s_service_cidr" {
-  type = string
-}
+# variable "k8s_service_cidr" {
+#   type = string
+# }
 
-variable "k8s_cluster_addons" {
-  type = list(object({
-    name   = string
-    config = string
-  }))
-}
+# variable "k8s_cluster_addons" {
+#   type = list(object({
+#     name   = string
+#     config = string
+#   }))
+# }
 
-variable "k8s_ops_role" {
-  type = string
-}
+# variable "k8s_ops_role" {
+#   type = string
+# }
 
 variable "db_envs_dev_sit" {
   type = list(string)
